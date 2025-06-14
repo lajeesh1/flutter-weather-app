0924239d79b281e3a7f9c6d9c959620825417b70
Lajeesh <gokulam.9920@gmail.com>
2025-06-14 13:23:21 +0530

    loading screen finished

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -13,10 +13,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Image.asset(
             'images/sun.png',
             height: 250,
+          ),
+          SizedBox(
+            height: 30,
           ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 55),
@@ -29,6 +34,36 @@ class _LoadingScreenState extends State<LoadingScreen> {
                 fontWeight: FontWeight.w600,
               ),
             ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 55),
+            child: Text(
+              textAlign: TextAlign.center,
+              "Displays current temperature, weather condition, humidity, and wind speed",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey.shade400,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 53,
+          ),
+          Container(
+            height: 55,
+            margin: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.blue,
+            ),
+            child: Center(
+                child: Text(
+              "Get Weather ",
+              style: TextStyle(fontSize: 18, fontFamily: "Poppins Regular"),
+            )),
           )
         ],
       ),
